badd565e595292c4eea8b96572ae8536199fd81c
Ian Christopherson <iechrist14@gmail.com>
2018-04-11 10:37:35 -0500

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/api/breweries/')
 def do_proxy():
 	args = request.url.split('?')[1]
-	res = req.get("https://api.brewerydb.com/v2/locations?{}".format(args) + "&key=94b47267204e747505d05fb3eaf543db&format=json")
+	res = req.get("https://api.brewerydb.com/v2/locations?{}".format(args) + API_KEY + "&format=json")
 	res = Response(res.text)
 	res.headers['Access-Control-Allow-Origin'] = '*'
 	res.headers['Content-type'] = 'application/json'
